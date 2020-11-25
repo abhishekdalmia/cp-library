04d2d172311a3076e8f8c428c9a412af9c742ace
Abhishek Dalmia <abhishekdalmia10@outlook.com>
2020-11-26 04:11:21 +0530

    Make basic skeleton.cpp

---
+++ skeleton.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#define fastio ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define pb push_back
+#define mp make_pair
+#define INF (int)1e9
+#define cBits(x) __builtin_popcount(x)
+#define gcd(a,b) __gcd((a),(b))
+#define lcm(a,b) a/gcd(a,b)*b
+#define FOR(i,a,b) for (int i=a;i<b;i++)
+#define FORr(i,a,b) for (int i=a;i>=b;i--)
+#define MAX3(a,b,c) max(a,max(b,c))
+#define MIN3(a,b,c) min(a,min(b,c))
+
+using namespace std;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+typedef pair<ll,ll> PLL;
+typedef pair<int,int> PII;
+
+const int MOD=1000000007;
+
+// solve(): solves an individual test case
+void solve() {
+
+	
+
+}
+
+int main() {
+
+	// fastio;
+	// #ifndef ONLINE_JUDGE
+	// 	freopen("input.txt", "r", stdin);
+	// 	freopen("output.txt", "w", stdout);
+	// #endif
+
+	int t = 1;
+	// multiple test cases:
+	// cin >> t;
+	while(t--) {
+		solve();
+	}
+
+	return 0;
+}
