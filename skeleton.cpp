d0ab95d8e6111086d2339794ce6498686dd33df7
Abhishek Dalmia <abhishekdalmia10@outlook.com>
2021-02-06 23:03:49 +0530

    Add function to check if two sorted vectors have a common element

--- skeleton.cpp
+++ skeleton.cpp
@@ -29,9 +29,27 @@ void pvec(const vector<T>& arr) {
 }
 
 template <typename T>
-void clearq(queue<T> &q) {
+void clearq(queue<T>& q) {
    queue<T> empty;
-   swap( q, empty );
+   swap(q, empty);
+}
+
+template <typename T>
+bool checkCommon(const vector <T>& v1, const vector <T>& v2) {
+    typename vector <T>::iterator it1 = v1.begin();
+    typename vector <T>::iterator it2 = v2.begin();
+    while(it1 != v1.end() && it2 != v2.end()) {
+        if (*it1 == *it2) {
+            return true;
+        }
+        if (*it1 < *it2) {
+            it1 = lower_bound(it1, v1.end(), *it2);
+        }
+        else if (*it2 < *it1) {
+            it2 = lower_bound(it2, v2.end(), *it1);
+        }
+    }
+    return false;
 }
 
 // solve(): solves an individual test case
