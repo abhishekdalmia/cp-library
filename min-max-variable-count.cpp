ba69d860e1337605c7dbfccd93325334fe1ed815
Abhishek Dalmia <abhishekdalmia10@outlook.com>
2021-08-03 12:32:05 +0530

    Update min-max-variable-count.cpp for max functionality

--- min-max-variable-count.cpp
+++ min-max-variable-count.cpp
@@ -12,4 +12,18 @@ auto min(const T1 &a, const T2 &b, const Args& ... args)
     return min(min(a, b), args...);
 }
 
+template <typename T1, typename T2>
+auto max(const T1 &a, const T2 &b)
+-> typename std::common_type<const T1&, const T2&>::type
+{
+    return a < b ? a : b;
+}
+
+template <typename T1, typename T2, typename ... Args>
+auto max(const T1 &a, const T2 &b, const Args& ... args)
+-> typename std::common_type<const T1&, const T2&, const Args& ...>::type
+{
+    return max(max(a, b), args...);
+}
+
 auto minimum = min(4, 5.8f, 3, 1.8, 3, 1.1, 9);
