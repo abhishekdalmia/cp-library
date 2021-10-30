d0eb8db87d4c71de6474e758201be30dbb9c4999
Abhishek Dalmia <abhishekdalmia10@outlook.com>
2021-10-30 16:47:27 +0530

    minor refactor of skeleton.cpp

--- skeleton.cpp
+++ skeleton.cpp
@@ -1,34 +1,30 @@
 #include <bits/stdc++.h>
-#define fastio ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 #define pb push_back
 #define mp make_pair
 #define INF (int)1e9
 #define cBits(x) __builtin_popcountll((ll)x)
-#define gcd(a,b) __gcd((a),(b))
-#define lcm(a,b) a/gcd(a,b)*(b)
-#define FOR(i,a,b) for (int i=a;i<b;i++)
-#define FORr(i,a,b) for (int i=a;i>=b;i--)
-#define MAX3(a,b,c) max(a,max(b,c))
-#define MIN3(a,b,c) min(a,min(b,c))
+#define gcd(a, b) __gcd((a), (b))
+#define lcm(a, b) a / gcd(a, b) * (b)
+#define FOR(i, a, b) for (int i = a; i < b; i++)
+#define FORr(i, a, b) for (int i = a; i >= b; i--)
+#define MAX3(a, b, c) max(a, max(b, c))
+#define MIN3(a, b, c) min(a, min(b, c))
 
 using namespace std;
 
 typedef long long ll;
 typedef unsigned long long ull;
 typedef pair<long long, long long> PLL;
-typedef pair<int,int> PII;
+typedef pair<int, int> PII;
 
-const int MOD=1000000007;
+const int MOD = 1000000007;
 
 // solve(): solves an individual test case
 void solve() {
-
-    
-
 }
 
 int main() {
-
     // fastio;
     // #ifndef ONLINE_JUDGE
     // 	freopen("input.txt", "r", stdin);
@@ -38,9 +34,9 @@ int main() {
     int t = 1;
     // multiple test cases:
     // cin >> t;
-    while(t--) {
+    while (t--) {
         solve();
     }
 
-	return 0;
+    return 0;
 }
