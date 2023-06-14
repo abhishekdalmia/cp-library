a77eb8737a16a85fe80e5c7606e1817e3d884384
Abhishek Dalmia <29015189+abhishekdalmia@users.noreply.github.com>
2023-06-14 23:53:40 +0530

    Update skeleton.cpp

--- skeleton.cpp
+++ skeleton.cpp
@@ -1,42 +1,34 @@
 #include <bits/stdc++.h>
-#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-#define pb push_back
-#define mp make_pair
-#define INF (int)1e9
+
+#define fastio ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
 #define cBits(x) __builtin_popcountll((ll)x)
 #define gcd(a, b) __gcd((a), (b))
 #define lcm(a, b) a / gcd(a, b) * (b)
-#define FOR(i, a, b) for (int i = a; i < b; i++)
-#define FORr(i, a, b) for (int i = a; i >= b; i--)
-#define MAX3(a, b, c) max(a, max(b, c))
-#define MIN3(a, b, c) min(a, min(b, c))
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+
+const int MOD = (int)1e9 + 7;
 
 using namespace std;
 
-typedef long long ll;
-typedef unsigned long long ull;
-typedef pair<long long, long long> PLL;
-typedef pair<int, int> PII;
+#ifndef ONLINE_JUDGE
+FILE* f1 = freopen("input.txt", "r", stdin);
+FILE* f2 = freopen("output.txt", "w", stdout);
+#endif
 
-const int MOD = 1000000007;
-
-// solve(): solves an individual test case
-void solve() {
+void solve()
+{
+	cout << "helloworld\n";
 }
 
-int main() {
-    // fastio;
-    // #ifndef ONLINE_JUDGE
-    // 	freopen("input.txt", "r", stdin);
-    // 	freopen("output.txt", "w", stdout);
-    // #endif
-
-    int t = 1;
-    // multiple test cases:
-    // cin >> t;
-    while (t--) {
-        solve();
-    }
-
-    return 0;
+int main()
+{
+	fastio;
+	int t = 1;
+	//cin >> t;
+	while (t--)
+	{
+		solve();
+	}
 }
